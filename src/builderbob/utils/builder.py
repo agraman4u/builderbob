1fe56dc078194328bf4a3653bab956e6a8c36be9
Aman Agrawal <agraman4u@gmail.com>
2024-06-25 03:07:04 +0530

    Implement Command overrides functionality to simplify build conf specification and other bug fixes

--- src/builderbob/utils/builder.py
+++ src/builderbob/utils/builder.py
@@ -10,27 +10,33 @@ class Builder:
 		self.config = config
 		self.build_conf = Config(config_file=PACKAGE_BUILD_CONFIG_FILE)
 
-	def _exec_build_command(self, cmd, *args):
-		build_target = self.config.get('target', required=True)
-		build_command = self.build_conf.get(cmd)[build_target]
+	def _get_build_command(self, cmd, build_target, *args):
+		assert cmd in self.build_conf.get("commands", required=True)[
+			build_target], f"{cmd} not found for {build_target}"
+
+		env = self.build_conf.get("build-system")[build_target]
+		command = cmd
+		try:
+			command = self.build_conf.get("commandOverrides")[cmd][build_target]
+		except KeyError:
+			pass
+
+		return " ".join([env, command, *args])
+
+	def _get_build_system(self, build_target):
+		return self.build_conf.get("build-system")[build_target]
+
+	def _get_build_target(self):
+		return self.config.get('target', required=True)
+
+	def execute(self, cmd, *args):
+		print("Building the package...")
+		build_target = self._get_build_target()
+
+		build_command = self._get_build_command(cmd, build_target, *args)
 		print("Running command", build_command)
 		result = subprocess.run(build_command, shell=True)
 		if result.returncode != 0:
 			raise Exception(f"Command {cmd} failed with code {result.returncode}")
+		print("Build completed successfully")
 		return result
-
-	def build(self):
-		# Simulate building process
-		print("Building the package...")
-		# Implement your build logic here
-		res = self._exec_build_command("build")
-		print("Build completed successfully")
-		return res
-
-	def release(self):
-		# Simulate building process
-		print("Building the package release...")
-		# Implement your build logic here
-		res = self._exec_build_command("release")
-		print("Build completed successfully")
-		return res
