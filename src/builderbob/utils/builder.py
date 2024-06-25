104a260991681b1a88fc62244eb8f6e0a3d62870
Aman Agrawal <115953119+agraman4u@users.noreply.github.com>
2024-06-26 05:20:59 +0530

    Enable custom build execution scripts (#12)

--- src/builderbob/utils/builder.py
+++ src/builderbob/utils/builder.py
@@ -2,7 +2,7 @@ import os
 import subprocess
 
 from builderbob.config import Config
-from builderbob.constants import PACKAGE_BUILD_CONFIG_FILE
+from builderbob.constants import PACKAGE_BUILD_CONFIG_FILE, CLIENT_ROOT_DIR
 
 
 class Builder:
@@ -11,6 +11,8 @@ class Builder:
 		self.build_conf = Config(config_file=PACKAGE_BUILD_CONFIG_FILE)
 
 	def _get_build_command(self, cmd, *args):
+		if self._get_build_system() not in self.build_conf.get("build-system").keys():
+			return self.get_custom_build_command(cmd, *args)
 		assert cmd in self.build_conf.get("commands", required=True)[
 			self._get_build_system()], f"{cmd} not found for {self._get_build_system()}"
 		build_system = self._get_build_system()
@@ -49,10 +51,17 @@ class Builder:
 	def _is_valid_client_config(self):
 		checks = [
 			self._build_validation_clause(
-				lambda: self._get_build_system() in self.build_conf.get("supportedLibraries")[self._get_build_target()],
-				f"Library '{self._get_build_target()}' is not support for build system '{self._get_build_system()}'")
+				lambda: self._get_build_system() in self.build_conf.get("supportedLibraries")[
+					self._get_build_target()] or os.path.exists(self.get_custom_build_path()),
+				f"Library '{self._get_build_target()}' is not supported/found for build system '{self._get_build_system()}'")
 		]
 		validation_res = list(filter(lambda x: x[0] != 0, map(lambda x: x(), checks)))
 		if not validation_res:
 			return True, []
 		return False, list(zip(*validation_res))[0]
+
+	def get_custom_build_command(self, cmd, *args):
+		return " ".join([self.get_custom_build_path(), cmd, *args])
+
+	def get_custom_build_path(self):
+		return os.path.join(CLIENT_ROOT_DIR, "bin", "build-scripts", self._get_build_system())
