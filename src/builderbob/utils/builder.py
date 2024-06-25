f44046085c4aa7507dbdba330927454863380089
Aman Agrawal <115953119+agraman4u@users.noreply.github.com>
2024-06-26 02:23:08 +0530

    Add flag parameter build system to enable multi platform setup for diiferent target libraries

--- src/builderbob/utils/builder.py
+++ src/builderbob/utils/builder.py
@@ -7,36 +7,52 @@ from builderbob.constants import PACKAGE_BUILD_CONFIG_FILE
 
 class Builder:
 	def __init__(self, config: Config):
-		self.config = config
+		self.client_config = config
 		self.build_conf = Config(config_file=PACKAGE_BUILD_CONFIG_FILE)
 
-	def _get_build_command(self, cmd, build_target, *args):
+	def _get_build_command(self, cmd, *args):
 		assert cmd in self.build_conf.get("commands", required=True)[
-			build_target], f"{cmd} not found for {build_target}"
-
-		env = self.build_conf.get("build-system")[build_target]
+			self._get_build_system()], f"{cmd} not found for {self._get_build_system()}"
+		build_system = self._get_build_system()
+		env = self.build_conf.get("build-system")[build_system]
 		command = cmd
 		try:
-			command = self.build_conf.get("commandOverrides")[cmd][build_target]
+			command = self.build_conf.get("commandOverrides")[cmd][build_system]
 		except KeyError:
 			pass
 
 		return " ".join([env, command, *args])
 
-	def _get_build_system(self, build_target):
-		return self.build_conf.get("build-system")[build_target]
+	def _get_build_system(self):
+		return self.client_config.get("build-system")
 
 	def _get_build_target(self):
-		return self.config.get('target', required=True)
+		return self.client_config.get('target', required=True)
 
 	def execute(self, cmd, *args):
 		print("Building the package...")
-		build_target = self._get_build_target()
 
-		build_command = self._get_build_command(cmd, build_target, *args)
+		is_valid, errors = self._is_valid_client_config()
+		assert is_valid, errors
+
+		build_command = self._get_build_command(cmd, *args)
 		print("Running command", build_command)
 		result = subprocess.run(build_command, shell=True)
 		if result.returncode != 0:
 			raise Exception(f"Command {cmd} failed with code {result.returncode}")
 		print("Build completed successfully")
 		return result
+
+	def _build_validation_clause(self, condition, error_msg):
+		return lambda: (-1, error_msg) if not condition() else (0, "")
+
+	def _is_valid_client_config(self):
+		checks = [
+			self._build_validation_clause(
+				lambda: self._get_build_system() in self.build_conf.get("supportedLibraries")[self._get_build_target()],
+				f"Library '{self._get_build_target()}' is not support for build system '{self._get_build_system()}'")
+		]
+		validation_res = list(filter(lambda x: x[0] != 0, map(lambda x: x(), checks)))
+		if not validation_res:
+			return True, []
+		return False, list(zip(*validation_res))[0]
